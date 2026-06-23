c9274d0c047ceb3583d07d213e084554c849e139
limiyama <lmizuyama@gmail.com>
2026-06-23 19:43:48 -0300

    css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Resume from './components/resume/Resume';
+import Portfolio from './components/portfolio/Portfolio';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Sidebar/>
+      <main className="main">
+        <Home/>
+        <About />
+        <Resume />
+        <Portfolio />
+      </main>
+    </> 
+  )
 }
 
 export default App;
