ebc408913341c378b767e527d7f542de42e27ba6
limiyama <lmizuyama@gmail.com>
2026-06-23 21:30:09 -0300

    aa

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+    return (
+        <div className="home_socials">
+            <a href="" className="home_social-link" target="_blank">
+                <i className="icon-social-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/ligia-mizuyama/" className="home_social-link" target="_blank">
+                <i className="icon-social-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/limiyama" className="home_social-link" target="_blank">
+                <i className="icon-social-github"></i>
+            </a>
+        </div>
+    )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./home.css";
+import Me from "../../assets/eu.png";
+import HeaderSocials from './HeaderSocials';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="home container" id="home">
+            <div className="intro">
+                <img src={Me} alt="" className="home_img" />
+                <h1 className="home_name">Lígia</h1>
+                <span className="home_education">I'm a undergraduate researcher!</span>
+
+                <HeaderSocials />
+
+                <a href="#contact" className="btn">Contact me!</a>
+            </div>
+        </section>
     )
 }
 
