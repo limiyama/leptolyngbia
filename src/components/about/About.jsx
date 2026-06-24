6391afd9872e55cff65f21360bedd14a1ddc5acc
limiyama <lmizuyama@gmail.com>
2026-06-23 21:44:48 -0300

    chega por hoje

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
 import "./about.css";
+import AboutMe from "../../assets/logo.png"
 
 const About = () => {
     return (
-        <div>About</div>
+        <section className="about container section" id="about">
+            <h2 className="section_title">
+                About me!!!
+            </h2>
+
+            <div className="about_container grid">
+                <img src={AboutMe} alt="" className="about_img" />
+
+                <div className="about_data grid">
+                    <div className="about_info">
+                        <p className="about_description"> pipipipipopopopo sou a lija</p>
+                        <a href="" className="btn">Download CV</a>
+                    </div>
+
+                    <div className="about_skills grid">
+                        <div className="skills_data">
+                            <div className="skills_titles">
+                                <h3 className="skills_name">Development </h3>
+                                <span className="skills_number development">90%</span>
+                            </div>
+
+                            <div className="skills_bar">
+                                <span className="skills_percentage"></span>
+                            </div>
+                        </div>
+
+                        <div className="skills_data">
+                            <div className="skills_titles">
+                                <h3 className="skills_name">UI/UX design</h3>
+                                <span className="skills_number">80%</span>
+                            </div>
+
+                            <div className="skills_bar">
+                                <span className="skills_percentage"></span>
+                            </div>
+                        </div>
+
+                        <div className="skills_data">
+                            <div className="skills_titles">
+                                <h3 className="skills_name">Suicidio</h3>
+                                <span className="skills_number">10000%</span>
+                            </div>
+
+                            <div className="skills_bar">
+                                <span className="skills_percentage"></span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
