b923705de28d282217c281b9c95a56c9136454ce
limiyama <lmizuyama@gmail.com>
2026-06-25 20:37:13 -0300

    menu

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image1 from "../../assets/logo.png"
+
+const Menu = [
+    {
+        id: 1,
+        image: Image1,
+        title: "Project Illustration",
+        category: "Design",
+    },
+    {
+        id: 2,
+        image: Image1,
+        title: "Project Illustration",
+        category: "Creative",
+    },
+    {
+        id: 3,
+        image: Image1,
+        title: "Project Illustration",
+        category: "Art",
+    },
+]
+
+export default Menu
